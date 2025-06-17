73d8fe2ac449ad98fa749e2638f069e3a7e5b47f
nguyen thu <nguyenthu180100@gmail.com>
2025-06-17 22:27:55 +0700

    init

---
+++ src/test/java/com/ThuT/TreasureHunter/TreasureHunterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ThuT.TreasureHunter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreasureHunterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
